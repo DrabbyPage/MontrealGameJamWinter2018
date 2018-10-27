82096d6930a0a6b0814a7216e00febb63ff5470d
DrabbyPage <35715195+DrabbyPage@users.noreply.github.com>
2018-10-26 23:11:16 -0400

    Peashooter Update
    Prefabs:
         arena is now prefab
         pea is a prefab
    
    Scripts:
         pea shooter script made
         pea has a script for movement and
              destruction

---
+++ Name TBD/Assets/Scripts/PeaScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeaScript : MonoBehaviour
+{
+    float moveSpeed = 10f;
+
+    float deathTime = 2f;
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        TimerToDestroy();
+        MoveBullet();
+	}
+
+    void MoveBullet()
+    {
+        gameObject.GetComponent<Rigidbody2D>().velocity = transform.up * moveSpeed;// AddForce(transform.up * moveSpeed);
+    }
+
+    void TimerToDestroy()
+    {
+        if(deathTime > 0)
+        {
+            deathTime = deathTime - Time.deltaTime;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        // should be the weapon
+        GameObject owner = gameObject.transform.parent.gameObject;
+
+        // should be the weapon's holder
+        GameObject ownersOwner = owner.transform.parent.gameObject;
+
+        if (col.gameObject.name != ownersOwner.name)
+        {
+            // push the object
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Name TBD/Assets/Scripts/PeaShooterScript.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeaShooterScript : MonoBehaviour
+{
+    GameObject bullet;
+
+    float joySense = 0.4f;
+    float time;
+
+    [SerializeField]
+    float fireTime = 0.1f;
+
+    bool canFire;
+
+    const string p1_LSH_Name = "P1_LJS_H";
+    const string p1_LSV_Name = "P1_LJS_V";
+    const string p1_RT_Name = "P1_RT";
+    const string p1_RSH_Name = "P1_RJS_H";
+    const string p1_RSV_Name = "P1_RJS_V";
+
+    // Use this for initialization
+    void Start ()
+    {
+        bullet = Resources.Load("Prefabs/Pea") as GameObject;
+
+        time = fireTime;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Shoot();
+	}
+    
+    void Shoot()
+    {
+        GameObject newBullet;
+
+        if(canFire)
+        {
+            if (Input.GetAxis(p1_RT_Name) > 0 && canFire)
+            {
+                newBullet = Instantiate(bullet) as GameObject;
+
+                newBullet.transform.eulerAngles = gameObject.transform.eulerAngles;
+
+                newBullet.transform.position = gameObject.transform.position + gameObject.transform.up;
+
+                canFire = false;
+            }
+        }
+        else
+        {
+            CheckFire();
+        }
+        
+    }
+
+    void CheckFire()
+    {
+        if(time > 0)
+        {
+            time = time - Time.deltaTime;
+        }
+        else
+        {
+            time = fireTime;
+            canFire = true;
+        }
+    }
+}
