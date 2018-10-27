a44770117dcf16c3f8045af6dad6f19d62ee4e40
DrabbyPage <35715195+DrabbyPage@users.noreply.github.com>
2018-10-27 14:22:53 -0400

    Explosion Update Spear Update
    explosion has a collider
    
    spear holder and spear have been made need implementation

---
+++ Name TBD/Assets/Scripts/PlayerTypes/SpearHolderScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpearHolderScript : MonoBehaviour
+{
+    bool hasWeapon = true;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void ThrowWeapon()
+    {
+        if(hasWeapon)
+        {
+            gameObject.transform.GetChild(0).GetComponent<SpearScript>().SetMove(true);
+        }
+    }
+}
---
+++ Name TBD/Assets/Scripts/SpearScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpearScript : MonoBehaviour
+{
+    bool canMove = false;
+
+    float spearMoveSpeed = 90;
+
+    GameObject parObj;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(canMove)
+        {
+            MoveSpear();
+        }
+	}
+
+    void MoveSpear()
+    {
+        gameObject.GetComponent<Rigidbody2D>().AddForce(gameObject.transform.up * spearMoveSpeed);
+    }
+
+    public void SetMove(bool newVal)
+    {
+        canMove = newVal;
+    }
+
+    public void SetParent(GameObject newPar)
+    {
+        parObj = newPar;
+    }
+}
