3d0f13396b4c4efc55096a6ac31447e5e895ff5c
DrabbyPage <35715195+DrabbyPage@users.noreply.github.com>
2018-10-27 20:55:32 -0400

    Scripts Moved Menus Updating

---
+++ Name TBD/Assets/Scripts/FlailScript.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlailScript : MonoBehaviour {
+
+    public float BoopForce = 100f;
+
+    [SerializeField]
+    float fullRotation = 360.0f;
+    [SerializeField]
+    float rotationAngle = -15.0f;
+    [SerializeField]
+    float increasedSpeed = -15.0f;
+    public bool spinFaster = false;
+
+    private Vector3 originalRot;
+
+    [SerializeField]
+    int cooldownTime = 60;
+    private int cooldownCounter = 0;
+
+    [SerializeField]
+    int allowedSpins = 4;
+    private int spinCounter = 0;
+
+    private int checker;
+    private int countUp = 0;
+
+    private void Start()
+    {
+        originalRot = transform.rotation.eulerAngles;
+        checker = Mathf.RoundToInt(Mathf.Abs(fullRotation / rotationAngle));
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (!spinFaster)
+        {
+            originalRot = transform.rotation.eulerAngles;
+        }
+
+        CheckCooldown();
+        ApplySpin();
+    }
+
+    // Determines whether or not the user can currently spin
+    private void CheckCooldown()
+    {
+        if (spinCounter == allowedSpins)
+        {
+            cooldownCounter++;
+
+            // If the player's cooldown is at max, allow the player to act again
+            if (cooldownCounter >= cooldownTime)
+            {
+                cooldownCounter = 0;
+                spinCounter = 0;
+            }
+        }
+    }
+
+    // Spins the held item
+    public void SpinFaster()
+    {
+        if (!spinFaster && spinCounter < allowedSpins)
+        {
+            spinFaster = true;
+            rotationAngle += increasedSpeed;
+        }
+    }
+
+    // Applies the spin to the held object
+    private void ApplySpin()
+    {
+        transform.Rotate(0, 0, rotationAngle);
+        countUp++;
+
+        if (countUp >= checker)
+        {
+            spinCounter++;
+
+            //transform.rotation = Quaternion.Euler(originalRot);
+            spinFaster = false;
+            rotationAngle -= increasedSpeed;
+            countUp = 0;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            //col.gameObject.GetComponent<Rigidbody2D>().AddForce(gameObject.transform.up * BoopForce);//, ForceMode2D.Force);
+            col.gameObject.GetComponent<BoopScript>().Booped(gameObject.transform.up, BoopForce);
+        }
+
+    }
+}
---
+++ Name TBD/Assets/Scripts/RandomizeManagerScript.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomizeManagerScript : MonoBehaviour
+{
+    bool play1Selected = true;
+    bool play2Selected = false;
+
+    Image P1_Char_1;
+    Image P1_Char_2;
+    Image P1_Char_3;
+
+    Image P2_Char_1;
+    Image P2_Char_2;
+    Image P2_Char_3;
+
+    Sprite randChar1Sprite;
+    Sprite randChar2Sprite;
+    Sprite randChar3Sprite;
+    Sprite randChar4Sprite;
+    Sprite randChar5Sprite;
+
+    Sprite randChar6Sprite;
+    Sprite randChar7Sprite;
+    Sprite randChar8Sprite;
+    Sprite randChar9Sprite;
+    Sprite randChar10Sprite;
+
+    Sprite randChar11Sprite;
+    Sprite randChar12Sprite;
+    Sprite randChar13Sprite;
+    Sprite randChar14Sprite;
+    Sprite randChar15Sprite;
+
+    // Use this for initialization
+    void Start()
+    {
+        randChar1Sprite = Resources.Load("Masks/HammerWhite") as Sprite;
+        randChar2Sprite = Resources.Load("Masks/HammerWhite") as Sprite;
+        randChar3Sprite = Resources.Load("Masks/HammerWhite") as Sprite;
+        randChar4Sprite = Resources.Load("Masks/HammerWhite") as Sprite;
+        randChar5Sprite = Resources.Load("Masks/HammerWhite") as Sprite;
+        randChar6Sprite = Resources.Load("Masks/HammerWhite") as Sprite;
+        randChar7Sprite = Resources.Load("Masks/HammerWhite") as Sprite;
+        randChar8Sprite = Resources.Load("Masks/HammerWhite") as Sprite;
+        randChar9Sprite = Resources.Load("Masks/HammerWhite") as Sprite;
+        randChar10Sprite = Resources.Load("Masks/HammerWhite") as Sprite;
+        randChar11Sprite = Resources.Load("Masks/HammerWhite") as Sprite;
+        randChar12Sprite = Resources.Load("Masks/HammerWhite") as Sprite;
+        randChar13Sprite = Resources.Load("Masks/HammerWhite") as Sprite;
+        //randChar14Sprite = Resources.Load("Masks/HammerWhite") as Sprite;
+        //randChar15Sprite = Resources.Load("Masks/HammerWhite") as Sprite;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void SwitchPlayers()
+    {
+        if(play1Selected)
+        {
+            play1Selected = false;
+            play2Selected = true;
+        }
+        else
+        {
+            play1Selected = true;
+            play2Selected = false;
+        }
+    }
+
+     
+}
