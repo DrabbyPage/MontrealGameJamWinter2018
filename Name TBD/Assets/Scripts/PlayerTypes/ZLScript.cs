658c9f0d390f8310bf010082c515c3661b4769ea
Walter Hill <walterhilldev@gmail.com>
2018-10-27 21:29:39 -0400

    added how to play scene

--- Name TBD/Assets/Scripts/PlayerTypes/ZLScript.cs
+++ Name TBD/Assets/Scripts/PlayerTypes/ZLScript.cs
@@ -37,8 +37,6 @@ public class ZLScript : MonoBehaviour {
     void FixedUpdate()
     {
         CheckCooldown();
-        ApplyCharge();
-
     }
 
     public void SetPlayerValues(GameObject player, Rigidbody2D rb)
